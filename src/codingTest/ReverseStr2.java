933e7327905850ec31c3f68e97b02e83a0ab1ef6
sangmok12 <amsangmok@gmail.com>
2023-07-04 17:30:50 +0900

    문자열 뒤집기

---
+++ src/codingTest/ReverseStr2.java
@@ -0,0 +1,11 @@
+package codingTest;
+
+public class ReverseStr2 {
+
+	public static void main(String[] args) {
+		String my_string = "jaron";
+		StringBuilder re = new StringBuilder(my_string).reverse();
+		String answer = re.toString();
+	}
+
+}
