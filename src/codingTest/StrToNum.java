aa38292a89ef538c03bb331425fd37ab2ac9e374
sangmok12 <amsangmok@gmail.com>
2023-10-10 15:52:06 +0900

    문자형태의 숫자 정수형으로 변

---
+++ src/codingTest/StrToNum.java
@@ -0,0 +1,15 @@
+package codingTest;
+
+public class StrToNum {
+
+	public static void main(String[] args) {
+		 int answer = 0;
+		 String s = "-1234";
+		 
+		 int sn = Integer.parseInt(s);
+		 
+		 System.out.println(sn);
+	       
+	}
+
+}
