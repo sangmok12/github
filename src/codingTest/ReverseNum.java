bd4f218e14405da0490ec82b989fa4a21e5bad2e
sangmok12 <amsangmok@gmail.com>
2023-10-13 22:05:11 +0900

    문자열을 뒤집는 메소드

--- src/codingTest/ReverseNum.java
+++ src/codingTest/ReverseNum.java
@@ -10,10 +10,18 @@ public class ReverseNum {
 		String num = ""+n;
 		
 		
+		
+		
 		int[] answer = new int[num.length()];
 		for (int i=0; i<num.length();i++) {
 			answer[num.length()-1-i] = Character.getNumericValue(num.charAt(i));
 		}
+		
+		
+		//문자열을 뒤집는 메소드 
+		StringBuilder numb = new StringBuilder(num);
+		numb = numb.reverse();
+		System.out.println(numb);
 		
 		
         System.out.println(Arrays.toString(answer));
