6721aa93a592c31b17e6155dfb80a12e180f02b2
sangmok12 <amsangmok@gmail.com>
2023-10-13 21:30:26 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ src/codingTest/XplusN.java
@@ -0,0 +1,21 @@
+package codingTest;
+
+import java.util.Arrays;
+
+public class XplusN {
+
+	public static void main(String[] args) {
+		 
+		int x =2;
+		int n =5;
+		
+		long[] answer = new long[n];
+	        
+	        for (int i=0; i<n; i++)
+	            answer[i] = x*(i+1);
+	        
+	        // 배열을 보려면 Arrays.toString 해줘야함.
+	        System.out.println(Arrays.toString(answer));
+	}
+
+}
