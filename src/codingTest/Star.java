90962130eee35f812724be62717868561e3b2eb0
sangmok12 <amsangmok@gmail.com>
2023-10-28 14:52:36 +0900

    Star

---
+++ src/codingTest/Star.java
@@ -0,0 +1,19 @@
+package codingTest;
+
+public class Star {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 3;
+		
+		
+		
+		for (int i=0;i<b;i++) {
+			for(int s=0;s<a;s++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
