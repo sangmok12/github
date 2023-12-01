a23f3715b7b5ef3dac55320050511f9e53a731e0
sangmok12 <amsangmok@gmail.com>
2023-12-01 14:02:11 +0900

    각 배열의 같은자리 더하기

---
+++ src/codingTest/ArrSum.java
@@ -0,0 +1,26 @@
+package codingTest;
+
+
+public class ArrSum {
+
+	public static void main(String[] args) {
+		int[][] a = {{1,2},{2,3}};
+		int[][] b = {{3,4},{5,6}};
+		
+		int alen = a.length;
+		
+		int[][] sum = new int[alen][a[0].length];
+		
+		for (int i=0;i<alen;i++) {
+			for (int s=0;s<a[i].length;s++) {
+				sum[i][s] = a[i][s] + b[i][s];
+				System.out.println(sum[i][s]);
+			}
+		}
+		
+		
+		
+		
+	}
+
+}
