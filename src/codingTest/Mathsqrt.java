5c58d7e879ed96bbc1aa60e20dde24c837e0623c
sangmok12 <amsangmok@gmail.com>
2023-12-04 17:14:40 +0900

    제곱인지 판별하기

---
+++ src/codingTest/Mathsqrt.java
@@ -0,0 +1,12 @@
+package codingTest;
+
+public class Mathsqrt {
+
+	public static void main(String[] args) {
+		int n = 144;
+		int answer = n%Math.sqrt(n)==0 ? 1:2;
+		System.out.println(answer);
+		
+	}
+
+}
