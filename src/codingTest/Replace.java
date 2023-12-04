e08778c7a3c4b49d0486811d0f7d5ecd28a6c5e4
sangmok12 <amsangmok@gmail.com>
2023-12-04 17:19:44 +0900

    Replace

---
+++ src/codingTest/Replace.java
@@ -0,0 +1,13 @@
+package codingTest;
+
+public class Replace {
+
+	public static void main(String[] args) {
+		String my_string="BCBdbe";
+		String letter = "B";
+		
+		String answer = my_string.replace(letter,"");
+		System.out.println(answer);
+	}
+
+}
